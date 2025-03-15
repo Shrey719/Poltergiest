41cab99540e60e646c3fa8bf9f629e6507d66926
shrey719 <shrey150436@gmail.com>
2025-03-15 15:57:51 -0400

    color scheme

--- src/routes/Settings.jsx
+++ src/routes/Settings.jsx
@@ -1,8 +1,15 @@
+import styles from '../Settings.module.css'
+
 function Settings() {
     return (
-    <>
-        TODO - make settings
-    </>)
+    <div>
+        <div class={styles.Navbar}>
+
+        </div>
+        <div class={styles.settingsBody}>
+            
+        </div>
+    </div>)
 }
 
 export default Settings
