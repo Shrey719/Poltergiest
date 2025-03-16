4b487883bcfc0bdf7744384f6759730f025d475f
shrey719 <shrey150436@gmail.com>
2025-03-15 20:16:23 -0400

    wip

--- src/routes/Settings.jsx
+++ src/routes/Settings.jsx
@@ -1,13 +1,16 @@
 import styles from '../Settings.module.css'
-
 function Settings() {
     return (
     <div>
         <div class={styles.Navbar}>
-
+            <div class={styles.Navbar_general}>
+                <button class={styles.Navbar_btn}>General</button>
+            </div>
+            <div class={styles.Navbar_connection}>
+                <button class={styles.Navbar_btn}>Connection</button>
+            </div>
         </div>
         <div class={styles.settingsBody}>
-            
         </div>
     </div>)
 }
