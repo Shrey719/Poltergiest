ff9e9babdd079798cdf52e897fe3183a2c427c79
shrey719 <shrey150436@gmail.com>
2025-03-18 20:57:55 -0400

    i did.. something?

--- src/routes/Settings.jsx
+++ src/routes/Settings.jsx
@@ -1,18 +1,53 @@
-import styles from '../Settings.module.css'
+import { createSignal } from 'solid-js';
+import styles from '../Settings.module.css';
+import {A} from "@solidjs/router"
+
 function Settings() {
+    const [sbcon, setsbCon] = createSignal('');
+
+    const openGen = () => setsbCon( 
+        <div id="gen">
+            TODO - General settings
+        </div>
+    );
+    const openCon = () => setsbCon(
+        <div id="con">
+            TODO - Connection (it should look like the about:settings connection for tor)
+        </div>
+    );
+    const openPrivacy = () => setsbCon(
+        <div id="priv">
+            TODO Privacy settings
+        </div>
+    );
+    const openSecurity = () => setsbCon(
+        <div id="sec">
+            TODO - security settings
+        </div>
+    ); 
+
     return (
-    <div>
-        <div class={styles.Navbar}>
-            <div class={styles.Navbar_general}>
-                <button class={styles.Navbar_btn}>General</button>
+        <div>
+            <div class={styles.Navbar}>
+                <A href="/"><img src="/back.svg" style="width: 2vw; height: 2vw; fill: white; margin-top: 5vh; margin-right: 15vw;"></img></A>
+                <div class={styles.Navbar_general}>
+                    <button class={styles.Navbar_btn} onClick={openGen}>General</button>
+                </div>
+                <div class={styles.Navbar_general}>
+                    <button class={styles.Navbar_btn} onClick={openCon}>Connection</button>
+                </div>
+                <div class={styles.Navbar_general}>
+                    <button class={styles.Navbar_btn} onClick={openPrivacy}>Privacy</button>
+                </div>
+                <div class={styles.Navbar_general}>
+                    <button class={styles.Navbar_btn} onClick={openSecurity}>Security</button> 
+                </div>
             </div>
-            <div class={styles.Navbar_connection}>
-                <button class={styles.Navbar_btn}>Connection</button>
+            <div class={styles.settingsBody} id="sbody">
+                {sbcon()} 
             </div>
         </div>
-        <div class={styles.settingsBody}>
-        </div>
-    </div>)
+    );
 }
 
-export default Settings
+export default Settings;
