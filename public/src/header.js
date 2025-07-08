6c56e32f234a8a8031710104b0d491aefc71abc1
Shrey719 <>
2025-07-08 12:43:19 -0400

    it does jack for now

--- public/src/header.js
+++ public/src/header.js
@@ -1,39 +1,50 @@
-function popupBar() {
-    let popup = css`
-        background-color: black;
-    `
-    return html`
-
-        <div class=${popup}>
-            
-        </div>
-    `
-}
-
 function header() {
-    let header = css`
-        height: 8vh;
-        background-color: ${$pol.header};
-        display: flex;
-        justify-content: center; 
-        align-items: center;   
+	let header = css`
+		height: 8vh;
+		background-color: ${$pol.header};
+		display: flex;
+		align-items: center;
+	`;
 
-    `
-    let popupBtn = css`
-        & button {
-            background-color: ${$pol.headerBtn};
-            border: none;
-            border-radius: 0.5rem;
-            height: 10rem;
-            width: 10rem;
-        }
-    `
-    return html`
-        <div class=${header}>
-            <div class=${popupBtn}>
-                <button>+</button>
-            </div>
-        </div>
-    `
+	let popupBtn = css`
+		border: none;
+		border-radius: 0.5rem;
+		height: 3rem;
+		width: 3rem;
+		margin-left: 1vw;
+		color: ${$pol.text};
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		cursor: pointer;
+
+		& svg {
+			height: 1.5rem;
+			width: 1.5rem;
+			fill: currentColor;
+		}
+	`;
+// pretti er (its actually pretty nice)
+	return html`
+		<div class=${header}>
+			<div
+				class=${popupBtn}
+				on:click=${() => {
+					alert(1);
+				}}
+			>
+				<svg
+					viewBox="0 0 24 24"
+					xmlns="http://www.w3.org/2000/svg"
+					aria-label="Menu"
+					role="img"
+				>
+					<rect y="4" width="24" height="2" />
+					<rect y="11" width="24" height="2" />
+					<rect y="18" width="24" height="2" />
+				</svg>
+			</div>
+		</div>
+	`;
 }
-document.body.append(header())
+document.body.append(header());
