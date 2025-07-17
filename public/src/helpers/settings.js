a3120bd5796ff9e884a92599b9df3ae94898f508
Shrey719 <>
2025-07-17 15:48:38 -0400

    le tism

--- public/src/helpers/settings.js
+++ public/src/helpers/settings.js
@@ -1,11 +1,5 @@
-function renderSettingsAreaCon() {
-    document.getElementById("settingsArea").innerHTML = ""
-    document.getElementById("settingsArea").append(html`
-    `)
-}
-function renderSettingsAreaGeneral() {
-    document.getElementById("settingsArea").innerHTML = ""
-    document.getElementById("settingsArea").append(html`
+function settingsAreaGeneral() {
+    return html`
         <div>
             <h1>Default Search engine</h1>
             <p>This is the search engine in search bar. <br> 
@@ -19,15 +13,15 @@ function renderSettingsAreaGeneral() {
                 <option value=${JSON.stringify({name: "Searx", url: "https://searxng.site?q="})}>Searx</option>
             </select>
         </div>
-    `)
+    `
 }
-function renderSettingsAreaPriv() {
-    document.getElementById("settingsArea").innerHTML = ""
-    document.getElementById("settingsArea").append(html`
+
+function settingsAreaPriv() {
+    return html`
         <div>
             <h1>Saftey level</h1>
             <select on:change=${() => {
-                localStorage.setItem("safetey", (event.target.value))
+                localStorage.setItem("safety", (event.target.value))
             }}>
             <option value="" disabled selected>Select a saftey level</option> 
             <option value="1">Unsafe</option>
@@ -35,11 +29,32 @@ function renderSettingsAreaPriv() {
             <option value="3">Anonymous (WIP)</option>
             <option value="4">Uncensored Anonymous (WIP)</option>
             </select>
-        </div>    
-    `)
+        </div> 
+    `
+}
+
+function settingsAreaCon() {
+    return html`
+    hi`
+}
+
+function renderSettingsAreaCon() {
+    document.getElementById("settingsArea").innerHTML = ""
+    document.getElementById("settingsArea").append(settingsAreaCon())
+}
+function renderSettingsAreaGeneral() {
+    document.getElementById("settingsArea").innerHTML = ""
+    document.getElementById("settingsArea").append(settingsAreaGeneral())
+}
+function renderSettingsAreaPriv() {
+    document.getElementById("settingsArea").innerHTML = ""
+    document.getElementById("settingsArea").append(settingsAreaPriv())
 }
 export {
     renderSettingsAreaCon,
     renderSettingsAreaGeneral,
-    renderSettingsAreaPriv
+    renderSettingsAreaPriv,
+    settingsAreaGeneral,
+    settingsAreaCon,
+    settingsAreaPriv
 }
--- public/src/settings.js
+++ public/src/settings.js
@@ -1,5 +1,7 @@
-import {settingsBar} from "./components/settingsSidebar.js"
-
+import {settingsBar} from "/src/components/settingsSidebar.js"
+import {
+    settingsAreaGeneral
+} from "/src/helpers/settings.js"
 function settings() {
     let main = css`
         height: 92vh;
@@ -30,34 +32,11 @@ function settings() {
         <div class=${main}>
             ${settingsBar()}
             <div id="settingsArea" class=${settingsArea}>
-                <div>
-                    <h1>Default Search engine</h1>
-                    <p>This is the search engine in search bar. <br> 
-                    You can switch it at any time.<p>
-                    <select on:change=${() => {
-                        localStorage.setItem("engine", (event.target.value))
-                    }}>
-                        <option value="" disabled selected>Select a search engine</option> 
-                        <option value=${JSON.stringify({name: "DuckDuckGo", url: "https://www.duckduckgo.com?q="})}>DuckDuckGo</option>
-                        <option value=${JSON.stringify({name: "Startpage", url: "https://www.startpage.com/sp/search?q="})}>Startpage</option>
-                        <option value=${JSON.stringify({name: "Searx", url: "https://searxng.site?q="})}>Searx</option>
-                    </select>
-                </div>
-                <div>
-                    <h1>Saftey level</h1>
-                    <select on:change=${() => {
-                        localStorage.setItem("safetey", (event.target.value))
-                    }}>
-                        <option value="" disabled selected>Select a saftey level</option> 
-                        <option value="1">Unsafe</option>
-                        <option value="2">Private</option>
-                        <option value="3">Anonymous (WIP)</option>
-                        <option value="4">Uncensored Anonymous (WIP)</option>
-                    </select>
-                </div>
+                ${settingsAreaGeneral()}
             </div>
         </div>
     `;
+
 }
 
 export default settings
